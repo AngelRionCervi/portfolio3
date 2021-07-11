cd8de885dd46ae8257f013609be49a9dd3db2f59
Angel <a.rioncervi@gmail.com>
2021-07-11 12:18:52 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import NavBar from "components/NavBar.svelte";
+</script>
+
+<main class="main">
+  <NavBar />
+</main>
+
+<style lang="scss">
+  @use 'theme' as *;
+  @import url('https://fonts.googleapis.com/css2?family=Karla:wght@200&display=swap');
+
+  :global(body) {
+    font-family: 'Karla', sans-serif;
+    background-color: get($colors, background);
+    margin: 0;
+    padding: 0;
+  }
+</style>
